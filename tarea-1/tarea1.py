0863ef78375579522019f5e4683ae4c2305ee53e
Gonzalo Moya <gemoya@inf.utfsm.cl>
2016-09-29 21:52:11 -0300

    arreglando

--- tarea-1/tarea1.py
+++ tarea-1/tarea1.py
@@ -3,46 +3,12 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA, KernelPCA
 from sklearn.datasets import make_swiss_roll
 from sklearn.manifold import Isomap
+from sklearn.lda import LDA
+from scipy.io import loadmat
+from tempfile import TemporaryFile
 
-
-def plot2d(X,color):
-    plt.figure()
-    plt.scatter(X_pca[:, 0], X_pca[:,1], marker='o', c=color)
-    plt.xlabel('PC1')
-    plt.ylabel('PC2')
-    plt.title('Cartesian coordinate PCA: ')
-    cbar = plt.colorbar()
-    cbar.set_label('?')
-    plt.show()
-
-
-
-X,color = make_swiss_roll(n_samples=3000, noise=0.05, random_state=None)
+X_sr,colors_sr = make_swiss_roll(n_samples=3000, noise=0.05, random_state=None)
 n_components = 2
-
-# PCA
-pca = PCA(n_components)
-X_pca = pca.fit_transform(X)
-
-# KernelPCA
-kpca = KernelPCA(n_components, kernel="poly", degree=2)
-X_kpca = kpca.fit_transform(X)
-
-# IsoMap
-#Y = Isomap(n_neighbors=10, n_components).fit_transform(X)
-
-
-## plot
-print color
-plt.figure()
-plt.scatter(X_pca[:, 0], X_pca[:,1], marker='o', c=color)
-plt.xlabel('PC1')
-plt.ylabel('PC2')
-plt.title('Cartesian coordinate PCA: ')
-cbar = plt.colorbar()
-cbar.set_label('?')
-plt.show()
-
-
-
-
+swissroll = TemporaryFile()
+np.savez(swissroll, X= X_sr, color=colors_sr)
+swissroll.seek(0)
